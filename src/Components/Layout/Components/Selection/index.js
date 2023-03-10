4f31fd919dffe2abc978172f94930c131640adb9
vuxthesong <vusong25@gmail.com>
2023-03-10 23:05:22 +0700

    selection ui

--- src/Components/Layout/Components/Selection/index.js
+++ src/Components/Layout/Components/Selection/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import classNames from "classnames/bind";
 import styles from "./selection.module.scss";
 import Button from "../Button";
+import { Back, Next } from "~/Components/Icons";
 
 const cx = classNames.bind(styles);
 function Selection() {
@@ -25,9 +26,17 @@ function Selection() {
 
   return (
     <div className={cx("wrapper")}>
-      {selection.map((text, index) => {
-        return <Button text={text} key={index} />;
-      })}
+      <div className={cx("icon-next")}>
+        <Next />
+      </div>
+      <div className={cx("selection")}>
+        {selection.map((text, index) => {
+          return <Button text={text} key={index} />;
+        })}
+      </div>
+      <div className={cx("icon-back")}>
+        <Back />
+      </div>
     </div>
   );
 }
